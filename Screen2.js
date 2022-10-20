6aa7af9ff2a62d257414e7eb4796a5d9e9fa7141
NVPL1234 <nlam39784@gmail.com>
2022-10-20 11:04:15 +0700

    finish hinh minh chung

--- Screen2.js
+++ Screen2.js
@@ -1,16 +1,31 @@
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image, Text, Button } from 'react-native';
 
-export default function Screen2 ({route}) {
+export default function Screen2({ route }) {
     return (
-        <View style={styles.container}>
-            <Image source={{uri: route.params.productsNew[0].url}} style={{ width: '50%', height: '50%', marginLeft: '30%', marginBottom: '20%' }}></Image>
+        <View>
+            <View style={{ backgroundColor: 'black' }}>
+                <Image source={{ uri: route.params.productsNew[0].url }} style={{ width: '50%', height: '50%', marginLeft: '30%', marginBottom: '20%' }}></Image>
+            </View>
+            <View style={{borderRadius: 10, borderTopWidth: 10}}>
+                <View>
+                    <Text style={{fontSize: 16}}>Calories: 344 kcal</Text>
+                    <Text style={{fontSize: 16}}>Angus bugger topped with lettuce, tomato and Shacksource</Text>
+                </View>
+                <View>
+                    <Text style={{fontSize: 16}}>Quantity</Text>
+                    <Button title='-'></Button>
+                    <Button title='+'></Button>
+                    <Button title='Customize'></Button>
+                    <Button title='Add to cart'></Button>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      marginTop: '5%',
-      padding: '1%'
+        marginTop: '5%',
+        padding: '1%'
     },
-  });
+});
